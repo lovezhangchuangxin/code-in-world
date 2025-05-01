000459fdd7d39cb0ab6394b48353dbba17ee5b47
lovezhangchuangxin <2911331070@qq.com>
2025-05-01 12:09:01 +0800

    feat: 增加一些地图工具

--- scripts/map/MapUtils.cs
+++ scripts/map/MapUtils.cs
@@ -1,5 +1,9 @@
 using Godot;
 
+/// <summary>
+/// 地图工具类
+/// 提供地图加载器和瓦片集的引用，以及一些常用的地图相关的计算方法
+/// </summary>
 public static class MapUtils
 {
     /// <summary>
@@ -13,12 +17,33 @@ public static class MapUtils
     public static TileSet TileSet { get; set; }
 
     /// <summary>
-    /// 瓦片集的大小
+    /// 瓦片的大小，即一个瓦片的长和宽是多少像素
     /// </summary>
-    public static Vector2 TileSize { get => TileSet.TileSize; }
+    public static Vector2I TileSize { get => TileSet.TileSize; }
 
     /// <summary>
-    /// 区块大小
+    /// 区块大小，即一个区块长和宽包含的瓦片数量
     /// </summary>
-    public static Vector2 ChunkSize { get => MapLoader.ChunkSize; }
+    public static Vector2I ChunkSize { get => MapLoader.ChunkSize; }
+
+    /// <summary>
+    /// 区块的像素大小，即一个区块的长和宽是多少像素
+    /// </summary>
+    public static Vector2I ChunkPixelSize { get; set; }
+
+    /// <summary>
+    /// 全局的瓦片坐标转为区块坐标
+    /// </summary>
+    public static Vector2I GlobalTileToChunk(Vector2I globalTileCoord)
+    {
+        return globalTileCoord / ChunkSize;
+    }
+
+    /// <summary>
+    /// 全局的瓦片坐标转为区块内的瓦片坐标
+    /// </summary>
+    public static Vector2I GlobalTileToLocal(Vector2I globalTileCoord)
+    {
+        return globalTileCoord % ChunkSize;
+    }
 }
